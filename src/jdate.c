b3ce2b2573c9f184c450633af1a71b6921566912
Max Base <MaxBaseCode@gmail.com>
2020-03-21 17:20:14 +0430

    Create jdate main file using c language

---
+++ src/jdate.c
@@ -0,0 +1,12 @@
+/**
+*
+* @Name : jdate.c
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2020-04-04
+* @Released under : https://github.com/BaseMax/jdate/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/jdate
+* @Related: https://github.com/BaseMax/ConvertCalendar
+* 			https://github.com/BaseMax/JavaJalaliCalendar
+*
+**/
